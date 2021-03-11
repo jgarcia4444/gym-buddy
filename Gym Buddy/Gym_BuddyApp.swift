4702c5371c04fad4f1c99cc3e5d563c536d30d7b
Jake Garcia <bballwiz4@gmail.com>
2021-03-10 19:29:33 -0800

    Initial Commit

---
+++ Gym Buddy/Gym_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_BuddyApp.swift
+//  Gym Buddy
+//
+//  Created by Jake Garcia on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Gym_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
