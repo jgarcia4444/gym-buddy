ff1b604c55f36ae29fa70abb67b6d607f5fc5692
Jake Garcia <bballwiz4@gmail.com>
2021-03-12 19:50:45 -0800

    The name field now check if they are left blank or contain numbers when the save button is clicked.

--- Gym Buddy/ProfileView.swift
+++ Gym Buddy/ProfileView.swift
@@ -13,9 +13,16 @@ struct ProfileView: View {
     @FetchRequest(entity: User.entity(), sortDescriptors: []) var users: FetchedResults<User>
     @State private var fName: String = ""
     @State private var lName: String = ""
+    @State private var email: String = ""
+    @State private var nameError: String = ""
     var body: some View {
         VStack {
             VStack(alignment: .leading) {
+                if nameError != "" {
+                    Text(nameError)
+                        .font(.subheadline)
+                        .foregroundColor(Color.red)
+                }
                 Text("Name")
                     .padding(.leading, 15)
                     .font(.headline)
@@ -37,10 +44,55 @@ struct ProfileView: View {
                 }
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 Divider()
+                VStack(alignment: .leading) {
+                    Text("Email")
+                        .padding(.leading, 15)
+                        .font(.headline)
+                    HStack {
+                    TextField("", text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .foregroundColor(metallicGold)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(metallicGold)
+                        )
+                    }
+                    .padding([.leading, .trailing], 10)
+                }
+                Divider()
+                VStack {
+                    Button(action: {
+                        self.checkUserInfo()
+                    }) {
+                        Text("Save")
+                    }
+                    
+                }
                 Spacer()
             }
         }
     }
+    
+    func checkUserInfo () {
+        self.validateName(name: fName)
+        self.validateName(name: lName)
+    }
+    
+    func validateName(name: String) {
+        if name.count == 0 {
+            nameError = "Name field cannot be left blank"
+            return
+        }
+        for char in fName {
+            let stringChar = String(char)
+            if let _ = Int(stringChar) {
+                nameError = "Name field cannot contain numbers"
+                return
+            }
+        }
+        nameError = ""
+    }
+    
 }
 
 struct ProfileView_Previews: PreviewProvider {
